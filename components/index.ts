65206d6b4c5795a9b25ed24fb9fd22ad4790c656
jacmanh <jacky.michy@gmail.com>
2023-10-16 20:49:08 +0200

    fix(app): export new components

--- components/index.ts
+++ components/index.ts
@@ -7,3 +7,8 @@ export { GlobalStyles } from './theme/style'
 
 /* COMPONENTS */
 export { Button } from './atoms/Button/Button'
+export { Checkbox } from './atoms/Checkbox/Checkbox'
+export { Radio } from './atoms/Radio/Radio'
+export { Toggle } from './atoms/Toggle/Toggle'
+export { Icon } from './atoms/Icon/Icon'
+export { Field } from './molecules/Field/Field'
