7bb62f71a3f5751770a1018d0870d489c4b97c14
jacmanh <jacky.michy@gmail.com>
2023-10-16 20:42:43 +0200

    feat(app): export colors

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 /* THEME */
 export { lightTheme } from './theme/lightTheme'
 export { darkTheme } from './theme/darkTheme'
+export { colors } from './theme/colors'
 
 export { GlobalStyles } from './theme/style'
 
